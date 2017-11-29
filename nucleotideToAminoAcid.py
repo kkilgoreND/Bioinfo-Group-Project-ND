da4eb4507b3e3dd801bf1861e89ac6ac0accd70e
Phil McCown <pjlmac@gmail.com>
2017-11-29 14:10:10 -0500

    More typos fixed

--- nucleotideToAminoAcid.py
+++ nucleotideToAminoAcid.py
@@ -14,7 +14,7 @@ control1amino=open("control1amino.fasta","w")
 control2amino=open("control2amino.fasta","w")
 obese1amino=open("obese1amino.fasta","w")
 obese2amino=open("obese2amino.fasta","w")
-build for loop that first ties the accession line and the sequence lines together
+#build for loop that first ties the accession line and the sequence lines together
 for fasta in control1:
     fasta=fasta.strip()
     if fasta[0] == ">":
@@ -26,7 +26,7 @@ control1.close()
 control2.close()
 obese1.close()
 obese2.close()
-control1-amino.close()
-control2-amino.close()
-obese1-amino.close()
-obese2-amino.close()
+control1amino.close()
+control2amino.close()
+obese1amino.close()
+obese2amino.close()
