d16b3dc12e4f6f722c6b038cdec51e90696a3547
Joe C <jchambe5@nd.edu>
2017-12-01 10:36:02 -0500

    added script

--- HMM/HMMsearch.sh
+++ HMM/HMMsearch.sh
@@ -1,3 +1,10 @@
 # a script for HMMsearch loop using the HMMbuild files and the fasta 
 files contianing sequences to be searched
+# bash HMMsearch.sh *.hmm
+# $1 is all the .hmm files in the directory
+
+for i in *.fasta
+do
+../hmmer-3.1b2-cygwin64/binaries/hmmsearch.exe --tblout $i.hits $1 $i
+done
 
