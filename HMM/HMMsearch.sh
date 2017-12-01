481a75a14b98d95364a1d3c27235a4829ce986c9
Joe C <jchambe5@nd.edu>
2017-12-01 12:42:00 -0500

    fixed a couple parts of the loop and added a # to stop confusing a comment for code

--- HMM/HMMsearch.sh
+++ HMM/HMMsearch.sh
@@ -1,9 +1,9 @@
 # a script for HMMsearch loop using the HMMbuild files and the fasta 
-files contianing sequences to be searched
+# files contianing sequences to be searched
 # bash HMMsearch.sh *.hmm
 # $1 is all the .hmm files in the directory
 
-for i in *.fasta
+for i in *amino.fasta
 do
 ../hmmer-3.1b2-cygwin64/binaries/hmmsearch.exe --tblout $i.hits $1 $i
 done
