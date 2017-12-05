1431992210c7b8f412164ac129f5e675a5961b77
Phil McCown <pjlmac@gmail.com>
2017-12-05 16:37:20 -0500

    Initial test of translation

---
+++ testTranslation.py
@@ -0,0 +1,63 @@
+import pandas
+import numpy
+import itertools
+from itertools import groupby
+control1=open("control1.fasta","r+")
+seqID=[]
+for line in control1:
+    line=line.strip()
+    if line[0] == ">":
+        seqID.append(line[1:])
+    else:
+        seqLen=float(len(line))
+def fasta_iter(control1):
+    """
+    given a fasta file. yield tuples of header, sequence
+    """
+    fh = open(control1)
+    # ditch the boolean (x[0]) and just keep the header or sequence since
+    # we know they alternate.
+    faiter = (x[1] for x in groupby(fh, lambda line: line[0] == ">"))
+    for header in faiter:
+        # drop the ">"
+        header = header.next()[1:].strip()
+        # join all sequence lines to one.
+        seq = "".join(s.strip() for s in faiter.next())
+        yield header, seq
+
+file = seq
+dna = file.read()
+
+print "DNA Sequence: ", dna
+
+# DNA codon table
+protein = {"TTT" : "F", "CTT" : "L", "ATT" : "I", "GTT" : "V",
+           "TTC" : "F", "CTC" : "L", "ATC" : "I", "GTC" : "V",
+           "TTA" : "L", "CTA" : "L", "ATA" : "I", "GTA" : "V",
+           "TTG" : "L", "CTG" : "L", "ATG" : "M", "GTG" : "V",
+           "TCT" : "S", "CCT" : "P", "ACT" : "T", "GCT" : "A",
+           "TCC" : "S", "CCC" : "P", "ACC" : "T", "GCC" : "A",
+           "TCA" : "S", "CCA" : "P", "ACA" : "T", "GCA" : "A",
+           "TCG" : "S", "CCG" : "P", "ACG" : "T", "GCG" : "A",
+           "TAT" : "Y", "CAT" : "H", "AAT" : "N", "GAT" : "D",
+           "TAC" : "Y", "CAC" : "H", "AAC" : "N", "GAC" : "D",
+           "TAA" : "STOP", "CAA" : "Q", "AAA" : "K", "GAA" : "E",
+           "TAG" : "STOP", "CAG" : "Q", "AAG" : "K", "GAG" : "E",
+           "TGT" : "C", "CGT" : "R", "AGT" : "S", "GGT" : "G",
+           "TGC" : "C", "CGC" : "R", "AGC" : "S", "GGC" : "G",
+           "TGA" : "STOP", "CGA" : "R", "AGA" : "R", "GGA" : "G",
+           "TGG" : "W", "CGG" : "R", "AGG" : "R", "GGG" : "G" 
+           }
+
+protein_sequence = ""
+
+# Generate protein sequence
+for i in range(0, len(dna)-(3+len(dna)%3), 3):
+    if protein[dna[i:i+3]] == "STOP" :
+        break
+    protein_sequence += protein[dna[i:i+3]]
+
+# Print the protein sequence
+print "Protein Sequence: ", protein_sequence
+
+# End of program
