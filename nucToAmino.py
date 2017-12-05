769154804124ef19d4c06176fb99bb7168e153d3
Phil McCown <pjlmac@gmail.com>
2017-12-05 13:21:30 -0500

    Modified variable designators to make more sense to people outside our group

--- nucToAmino.py
+++ nucToAmino.py
@@ -3,29 +3,23 @@ import pandas
 import numpy
 import re
 
-#open file codonmap and store it as a dictionary under the variable name "d"
-d = {}
+#open the codonmap and store it as a dictionary
+dictionary = {}
 with open('codonmap.txt', 'r') as csv_file:
     for line in csv_file:
-        aa, codon = line.split() # Don't need parentheses for simultaneous assignment
-        d[codon] = aa
+        amino, codon = line.split()
+        dictionary[codon] = amino
 
 def translate(codex, fasta):
-    """
-    When passed a full fasta file split by line (i.e. file.read().split()),
-    this function translates DNA to Protein up to an
-    Amber (TAG), Ochre (TAA), or Umber (TGA) stop codon.
-    Returns a list of tab delimited NAME-SEQUENCE pairs.
-    """
     sequences = [] # sequential list of protein sequences
     sequence_names = []
-    for i, item in enumerate(fasta.read().split()): # loops over list, list items
+    for carat, item in enumerate(fasta.read().split()): # built a for loop to loop through lists
         protein = '' # translated protein sequence
-        if i%2 == 0: # if index is even
+        if carat%2 == 0: # if index is even
             sequence_names.append(item)
         else:
-            for j in range(0, len(item), 3):
-                res = codex[item[j:j+3]]
+            for aprotein in range(0, len(item), 3):
+                res = codex[item[aprotein:aprotein+3]]
                 if res == 'Stop':
                     break
                 else:
@@ -38,4 +32,4 @@ if __name__ == '__main__':
     condition_list = ['control1', 'control2', 'obese1', 'obese2']
     for condition in condition_list:
         with open('%s.fasta'%condition, 'r') as inFile, open('%sprotein.fasta'%condition, 'w') as outFile:
-            outFile.write(translate(d, inFile))
+            outFile.write(translate(dictionary, inFile))
