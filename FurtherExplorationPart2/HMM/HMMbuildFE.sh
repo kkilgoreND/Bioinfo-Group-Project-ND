febb8cacce278b278c33c4b4b4b2b31febe4b04f
ayamasaki2011 <ayamasak@nd.edu>
2017-12-06 17:53:21 -0500

    Built HMM models for all new protein fasta files in Further Exploration Part 2

---
+++ FurtherExplorationPart2/HMM/HMMbuildFE.sh
@@ -0,0 +1,7 @@
+# HMM build loop using alignments from muscle script (.align files) and output as .hmm file
+
+for i in *.align
+do
+../../HMMandMuscle/hmmer-3.1b2-cygwin64/binaries/hmmbuild.exe $i.hmm $i
+done
+
