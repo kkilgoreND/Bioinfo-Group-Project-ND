fdc09551802a878f01da2d78b38acdd1c3aa8152
Phil McCown <pjlmac@gmail.com>
2017-12-06 16:35:18 -0500

    GGPlot done; qualitative writeup done

--- GGPlotCounts/plot.py
+++ GGPlotCounts/plot.py
@@ -8,6 +8,11 @@ from plotnine import *
 ProteinDF=pandas.read_csv("ProteinCounts.csv",header=0)
 #make shape so that ggplot can read file
 ProteinDF.shape
-#ggplot lines
-a=ggplot(ProteinDF,aes(x="Protein",y1="Control1",y2="Control2",y3="Obese1",y4="Obese2))
-a+ggplot
+a=ggplot(ProteinDF,aes(x="Protein",y="Control1"))+geom_point(color="blue")
+a+theme_classic()
+b=ggplot(ProteinDF,aes(x="Protein",y="Control2"))+geom_point(color="red")
+b+theme_classic()
+c=ggplot(ProteinDF,aes(x="Protein",y="Obese1"))+geom_point(color="green")
+c+theme_classic()
+d=ggplot(ProteinDF,aes(x="Protein",y="Obese2"))+geom_point(color="black")
+d+theme_classic()
