69480610edbad3890897c29b46b87be4188a8167
Phil McCown <pjlmac@gmail.com>
2017-12-06 14:07:49 -0500

    Some progress on the plot.py script.

--- GGPlotCounts/plot.py
+++ GGPlotCounts/plot.py
@@ -4,11 +4,7 @@ import pandas
 import scipy
 from plotnine import *
 
-#set variables/dataframes for RNAseq files
-1=pandas.read_csv("control1",sep="t",header=0)
-2=pandas.read_csv("control2",sep="t",header=0)
-3=pandas.read_csv("obese1",sep="t",header=0)
-4=pandas.read_csv("obese2",sep="t",header=0)
-
-#combine control groups and obese groups into one dataframe
-plotdf=pandas.DataFrame(list(zip(control1, control2, obese1, obese2)),columns=['1','2','3','4'])
+#assign ProteinCounts CSV as dataFrame
+Protein=pandas.read_csv("ProteinCounts.csv",header=0)
+#make shape so that ggplot can read file
+Protein.shape
