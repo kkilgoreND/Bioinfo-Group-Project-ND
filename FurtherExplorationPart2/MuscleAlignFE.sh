d94147dc5c8505f5fdd152cd550c8b412f2e62da
ayamasaki2011 <ayamasak@nd.edu>
2017-12-06 17:44:47 -0500

    Made new ProteinBlast files for Further Exploration Part 2 from more diverse species; ran alignment on all new FASTA files in the directory

---
+++ FurtherExplorationPart2/MuscleAlignFE.sh
@@ -0,0 +1,9 @@
+#loop to align 6 transcripts from downloaded protein sequences
+#this loop takes protein transcripts from ProteinBlast directory and outputs to that directory
+#we then copy the .align file to the HMM directory for ease with HMM commands in the future
+
+for i in ./ProteinBlast/*.fasta
+do
+../HMMandMuscle/muscle3.8.31_i86win32.exe -in $i -out $i.align
+cp ./ProteinBlast/*.align ./HMM
+done
