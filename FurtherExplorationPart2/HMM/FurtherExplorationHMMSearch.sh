5bfcb36f14bedc122416da99c4d294fecaf60db9
Phil McCown <pjlmac@gmail.com>
2017-12-07 13:42:00 -0500

    Ran HMMsearch script and generated .hits files

---
+++ FurtherExplorationPart2/HMM/FurtherExplorationHMMSearch.sh
@@ -0,0 +1,6 @@
+../../HMMandMuscle/hmmer-3.1b2-cygwin64/binaries/hmmsearch.exe --tblout Atp12a.hits Atp12a.2.fasta.align.hmm ../obese1protein
+../../HMMandMuscle/hmmer-3.1b2-cygwin64/binaries/hmmsearch.exe --tblout Gsta2.hits Gsta2.2.fasta.align.hmm ../obese1protein
+../../HMMandMuscle/hmmer-3.1b2-cygwin64/binaries/hmmsearch.exe --tblout Lhx2.hits Lhx2.2.fasta.align.hmm ../obese1protein
+../../HMMandMuscle/hmmer-3.1b2-cygwin64/binaries/hmmsearch.exe --tblout Ptpn5.hits Ptpn5.2.fasta.align.hmm ../obese1protein
+../../HMMandMuscle/hmmer-3.1b2-cygwin64/binaries/hmmsearch.exe --tblout Slc7a12.hits Slc7a12.2.fasta.align.hmm ../obese1protein
+../../HMMandMuscle/hmmer-3.1b2-cygwin64/binaries/hmmsearch.exe --tblout Synpr.hits Synpr.2.fasta.align.hmm ../obese1protein
